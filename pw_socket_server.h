2783d23072dcd6df7dbaef6b089b315af11fda6d
templar1982 <ctemple@21cn.com>
2013-03-10 11:36:49 +0800

    Create pw_socket_server.h

---
+++ pw_socket_server.h
@@ -0,0 +1,17 @@
+#ifndef _pw_socket_server_
+#define _pw_socket_server_
+
+namespace pwutils
+{
+    class SocketServer
+    {
+    public:
+        class Listener
+        {
+        };
+    };
+
+  
+}
+
+#endif // _pw_socket_server_
